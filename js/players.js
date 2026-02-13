8d246c37333e50cb9e778deb3e1f9d0dcb39088b
byishimo58-byte <byishimo58@gmail.com>
2026-02-13 18:59:06 +0100

    Create players.js

---
+++ js/players.js
@@ -0,0 +1,163 @@
+const players = [
+  {
+    name: "Kylian Mbappé",
+    rating: 91,
+    position: "ST",
+    club: "Paris Saint-Germain",
+    nationality: "France",
+    card: "images/cards/mbappe_gold.png"
+  },
+  {
+    name: "Erling Haaland",
+    rating: 90,
+    position: "ST",
+    club: "Manchester City",
+    nationality: "Norway",
+    card: "images/cards/haaland_toty.png"
+  },
+  {
+    name: "Lionel Messi",
+    rating: 91,
+    position: "RW",
+    club: "Inter Miami",
+    nationality: "Argentina",
+    card: "images/cards/messi_icon.png"
+  },
+  {
+    name: "Kevin De Bruyne",
+    rating: 91,
+    position: "CM",
+    club: "Manchester City",
+    nationality: "Belgium",
+    card: "images/cards/debruyne_gold.png"
+  },
+  {
+    name: "Robert Lewandowski",
+    rating: 90,
+    position: "ST",
+    club: "Barcelona",
+    nationality: "Poland",
+    card: "images/cards/lewandowski_gold.png"
+  },
+  {
+    name: "Vinícius Júnior",
+    rating: 90,
+    position: "LW",
+    club: "Real Madrid",
+    nationality: "Brazil",
+    card: "images/cards/vini_jr_gold.png"
+  },
+  {
+    name: "Joshua Kimmich",
+    rating: 89,
+    position: "CDM",
+    club: "Bayern Munich",
+    nationality: "Germany",
+    card: "images/cards/kimmich_gold.png"
+  },
+  {
+    name: "Trent Alexander‑Arnold",
+    rating: 88,
+    position: "RB",
+    club: "Liverpool",
+    nationality: "England",
+    card: "images/cards/taar_gold.png"
+  },
+  {
+    name: "Luis Suárez",
+    rating: 87,
+    position: "ST",
+    club: "Nacional",
+    nationality: "Uruguay",
+    card: "images/cards/suarez_gold.png"
+  },
+  {
+    name: "Bruno Fernandes",
+    rating: 88,
+    position: "CAM",
+    club: "Manchester United",
+    nationality: "Portugal",
+    card: "images/cards/fernandes_gold.png"
+  },
+  {
+    name: "Harry Kane",
+    rating: 88,
+    position: "ST",
+    club: "Bayern Munich",
+    nationality: "England",
+    card: "images/cards/kane_gold.png"
+  },
+  {
+    name: "Neymar Jr.",
+    rating: 89,
+    position: "LW",
+    club: "Al Hilal",
+    nationality: "Brazil",
+    card: "images/cards/neymar_gold.png"
+  },
+  {
+    name: "Luka Modrić",
+    rating: 88,
+    position: "CM",
+    club: "Real Madrid",
+    nationality: "Croatia",
+    card: "images/cards/modric_gold.png"
+  },
+  {
+    name: "Jan Oblak",
+    rating: 91,
+    position: "GK",
+    club: "Atletico Madrid",
+    nationality: "Slovenia",
+    card: "images/cards/oblak_gold.png"
+  },
+  {
+    name: "Sergio Ramos",
+    rating: 87,
+    position: "CB",
+    club: "Sevilla",
+    nationality: "Spain",
+    card: "images/cards/ramos_gold.png"
+  },
+  {
+    name: "Alphonso Davies",
+    rating: 88,
+    position: "LB",
+    club: "Bayern Munich",
+    nationality: "Canada",
+    card: "images/cards/davies_gold.png"
+  },
+  {
+    name: "Phil Foden",
+    rating: 87,
+    position: "CAM",
+    club: "Manchester City",
+    nationality: "England",
+    card: "images/cards/foden_gold.png"
+  },
+  {
+    name: "Rúben Dias",
+    rating: 88,
+    position: "CB",
+    club: "Manchester City",
+    nationality: "Portugal",
+    card: "images/cards/dias_gold.png"
+  },
+  {
+    name: "Son Heung‑min",
+    rating: 88,
+    position: "LW",
+    club: "Tottenham",
+    nationality: "South Korea",
+    card: "images/cards/son_gold.png"
+  },
+  {
+    name: "Rodri",
+    rating: 89,
+    position: "CDM",
+    club: "Manchester City",
+    nationality: "Spain",
+    card: "images/cards/rodri_gold.png"
+  }
+  // Continue adding more players here…
+];
