09292b7495b441f1c68f6e3dfeac5dc02b2498bd
byishimo58-byte <byishimo58@gmail.com>
2026-02-13 18:41:45 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,1 @@
-const players = [
-  { name: "Mbappe", rating: 91, position: "ST", league: "Ligue 1", type: "Gold", pace: 97, shooting: 89, passing: 80, dribbling: 92 },
-  { name: "Haaland", rating: 91, position: "ST", league: "Premier League", type: "Gold", pace: 88, shooting: 93, passing: 70, dribbling: 80 },
-  { name: "Ronaldinho", rating: 94, position: "CAM", league: "Icons", type: "Icon", pace: 90, shooting: 91, passing: 92, dribbling: 95 },
-  { name: "Neymar", rating: 91, position: "LW", league: "Ligue 1", type: "Gold", pace: 91, shooting: 85, passing: 87, dribbling: 95 },
-  { name: "De Bruyne", rating: 91, position: "CM", league: "Premier League", type: "Gold", pace: 76, shooting: 86, passing: 93, dribbling: 88 }
-];
+
