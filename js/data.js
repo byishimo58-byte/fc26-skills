f10de48441f4681f379a132cddef8b2f975b633a
byishimo58-byte <byishimo58@gmail.com>
2026-02-13 18:42:45 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,8 @@
-
+const players1 = [
+  { name: "Mbappé", rating: 91, position: "ST", card: "images/cards/mbappe_gold.png" },
+  { name: "Haaland", rating: 90, position: "ST", card: "images/cards/haaland_toty.png" },
+  { name: "Messi", rating: 91, position: "RW", card: "images/cards/messi_icon.png" },
+  { name: "Ronaldo", rating: 91, position: "ST", card: "images/cards/ronaldo_icon.png" },
+  { name: "Bellingham", rating: 89, position: "CM", card: "images/cards/bellingham_gold.png" },
+  // ...add up to 500 players
+];
